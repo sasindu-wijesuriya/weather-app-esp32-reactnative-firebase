185b93a93aed86c2c02eda7d75c576c950fa9ec5
Sasindu Wijesuriya <sasindu@ackcio.com>
2024-08-01 14:10:32 +0530

    Add dynamic temperature, humidity, and pressure values from Firebase

--- screens/Weather.js
+++ screens/Weather.js
@@ -1,24 +1,39 @@
 import { StyleSheet, Text, View, ImageBackground } from "react-native";
-import React from "react";
+import { useState, useEffect } from "react";
+import { db, ref, onValue } from "../firebaseData";
 
 const Weather = () => {
+  const [temperatureValue, setTemperatureValue] = useState(0);
+  const [humidityValue, setHumidityValue] = useState(0);
+  const [pressureValue, setPressureValue] = useState(0);
+
+  useEffect(() => {
+    const dbRef = ref(db);
+    onValue(dbRef, (snapshot) => {
+      const data = snapshot.val();
+      setTemperatureValue(data.temperatureData);
+      setHumidityValue(data.humidityData);
+      setPressureValue(data.pressureData);
+    });
+  }, [db]);
+
   return (
     <ImageBackground
       source={require("../assets/background1.png")}
       style={styles.container}
     >
       <View style={styles.temperatureWrapper}>
-        <Text style={styles.tempText}>75 C</Text>
+        <Text style={styles.tempText}>{temperatureValue} °F</Text>
       </View>
       <View style={styles.data}>
         <View style={styles.spacer}></View>
         <View style={styles.dataWrapper}>
           <View style={styles.humidity}>
-            <Text style={styles.dataValue}>75%</Text>
+            <Text style={styles.dataValue}>{humidityValue}%</Text>
             <Text style={styles.dataText}>Humidity</Text>
           </View>
           <View style={styles.pressure}>
-            <Text style={styles.dataValue}>1000 hPa</Text>
+            <Text style={styles.dataValue}>{pressureValue} hPa</Text>
             <Text style={styles.dataText}>Pressure</Text>
           </View>
         </View>
@@ -43,7 +58,8 @@ const styles = StyleSheet.create({
     fontSize: 120,
     color: "#fff",
     fontWeight: "100",
-    textAlign: "center",
+    textAlign: "right",
+    marginRight: "10%",
   },
   data: {
     flex: 1,
